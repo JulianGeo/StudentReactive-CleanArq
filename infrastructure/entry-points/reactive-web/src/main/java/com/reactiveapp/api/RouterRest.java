d62f5dc5e44daaf4886423b62aa5e3f8523f9a11
Julian Rojas <julianrojasgeo@gmail.com>
2023-03-30 16:41:06 -0500

    swagger working for get all students

--- infrastructure/entry-points/reactive-web/src/main/java/com/reactiveapp/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/reactiveapp/api/RouterRest.java
@@ -3,10 +3,16 @@ package com.reactiveapp.api;
 import com.reactiveapp.model.student.Student;
 import com.reactiveapp.usecase.getallstudents.GetAllStudentsUseCase;
 import com.reactiveapp.usecase.savestudent.SaveStudentUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -24,6 +30,13 @@ public class RouterRest {
     }*/
 
     @Bean
+    @RouterOperation(path = "/api/students", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = GetAllStudentsUseCase.class, method = RequestMethod.GET, beanMethod = "get",
+            operation = @Operation(operationId = "getAllStudents", tags = "Student usecases", responses = {
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            content = @Content(schema = @Schema(implementation = Student.class))),
+                    @ApiResponse(responseCode = "204", description = "Nothing to show")}))
     public RouterFunction<ServerResponse> getAllStudents(GetAllStudentsUseCase getAllStudentsUseCase){
         return route(GET("/api/students"),
                 request -> ServerResponse.status(201)
